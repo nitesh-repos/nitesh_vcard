3eed31c1e49eba1a26721c3ac012303384d09bc7
nitesh-repos <niteshnanotkar@gmail.com>
2020-11-08 15:40:37 +0530

    Second stage of vCard

--- main.py
+++ main.py
@@ -9,9 +9,23 @@ def main_render():
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
+        conn = sqlite3.connect('request.db')
+        try:
+            with sqlite3.connect("database.db") as conn:
+                cur = conn.cursor()
+                cur.execute('CREATE TABLE IF NOT EXISTS usr_request (entryID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, msg TEXT)')
+                cur.execute(f'INSERT INTO usr_request (name, email, msg) VALUES ("{name}","{email}","{message}")')
+                conn.commit()
+                msg = "Record successfully added"
+        except:
+            conn.rollback()
+            msg = "error in insert operation"
+        finally:
+            conn.close()
     else:
         return render_template('index.html')
         # return redirect(url_for('success',name = user))
+
 if __name__ == '__main__':
     # app.run()
     # app.debug = True
